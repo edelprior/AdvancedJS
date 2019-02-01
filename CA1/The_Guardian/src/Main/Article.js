65ff42fc44f20a19883e4d37324cb93b94242c6a
edelprior <edelprior@hotmail.com>
2019-02-01 18:39:24 +0000

    Requirements Completed

--- CA1/The_Guardian/src/Main/Article.js
+++ CA1/The_Guardian/src/Main/Article.js
@@ -1,11 +1,10 @@
 import React from "react";
-import NewsCard from "./Components/NewsCard";
-import DropDown from "./Components/DropDown";
-import LabelledInput from "./Components/LabelledInput";
-import NavBar from "./Components/NavBar";
+import NewsCard from "../Components/NewsCard";
+import LabelledInput from "../Components/LabelledInput";
+import DropDown from "../Components/DropDown";
 import axios from "axios";
-//import Router from "Router";
-class App extends React.Component {
+
+class Article extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -14,18 +13,16 @@ class App extends React.Component {
       searchText: ""
     };
     this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
     axios
       .get(
-        // "https://content.guardianapis.com/tags?q=brexit&api-key=b21e1e64-196e-4742-b2b9-5d8ebdbdf57d"
         "https://content.guardianapis.com/search?api-key=b21e1e64-196e-4742-b2b9-5d8ebdbdf57d"
       )
-      .then(responseArray => {
+      .then(response => {
         this.setState({
-          articles: responseArray.data.response.results
+          articles: response.data.response.results
         });
       })
       .catch(error => {
@@ -34,54 +31,39 @@ class App extends React.Component {
   }
 
   handleChange(event) {
-
     const target = event.target;
     const value = target.value;
     const name = target.name;
-
 
     this.setState({
       [name]: value
     });
   }
 
-  handleClick(event) {
-
-    const name = event.target.name;
-    this.setState(prevState => ({
-      [name]: !prevState[name]
-    }));
-  }
-
   render() {
-
-      let articleList = this.state.articles.map(article => {
+    let articleList = this.state.articles.map(article => {
       const sectionMatch =
         this.state.sectionSelected === article.pillarName ||
         this.state.sectionSelected === "all";
 
-      const searchMatch = article.webTitle.toLowerCase().startsWith(this.state.searchText.toLowerCase());
+      const searchMatch = article.webTitle
+        .toLowerCase()
+        .startsWith(this.state.searchText.toLowerCase());
       return sectionMatch && searchMatch ? (
         <NewsCard
           key={article.id}
           title={article.webTitle}
           name={article.pillarName}
           section={article.sectionName}
+          link={article.webUrl}
         />
       ) : null;
     });
     return (
-
-      <section>
-      <BrowserRouter>
       <div>
-      <h1> 
-
-        <div className="column is multiline">
-
-        <div className = "column is-4">
+        <div className="column is-6">
           <DropDown
-            options={["all", "News", "Sport", "Arts", "Opinion"]}
+            options={["All", "News", "Sport", "Arts", "Opinion", "Lifestyle"]}
             handleChange={this.handleChange}
             label="Filter by Section"
             name="sectionSelected"
@@ -92,15 +74,12 @@ class App extends React.Component {
             label="Search by Title"
             value={this.state.searchText}
             handleChange={this.handleChange}
-            placeholder={"e.g. Brexit"}
+            placeholder={"e.g. Weather"}
           />
-          </div>
-
-          <div className="columns is-multiline">{articleList}</div>
         </div>
-      </section>
-
+        <div className="columns is-multiline">{articleList}</div>
+      </div>
     );
   }
 }
-export default App;
+export default Article;
--- CA1/The_Guardian/src/index.js
+++ CA1/The_Guardian/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter,Route,Link}from 'react-router-dom';
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
