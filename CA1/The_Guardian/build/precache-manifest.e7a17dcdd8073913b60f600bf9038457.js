a88cef724a1578ca1d1cf4dfda213b61cedf9c6e
edelprior <edelprior@hotmail.com>
2019-02-05 20:51:21 +0000

    deploy

---
+++ CA1/The_Guardian/build/precache-manifest.e7a17dcdd8073913b60f600bf9038457.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9063f6418c4e739ed5c",
+    "url": "/static/js/main.f9063f64.chunk.js"
+  },
+  {
+    "revision": "7731dbf5c7b06b8f4108",
+    "url": "/static/js/1.7731dbf5.chunk.js"
+  },
+  {
+    "revision": "148ffba3b3618328241250900a8347c0",
+    "url": "/index.html"
+  }
+];
